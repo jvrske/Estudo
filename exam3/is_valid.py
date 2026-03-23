d64e7dac7cfec0df51a27f5b120c84aecbdb25cc
csilva <jarskewow@hotmail.com>
2026-03-23 17:30:48 +0000

    v1

---
+++ exam3/is_valid.py
@@ -0,0 +1,17 @@
+def is_valid(s: str) -> bool:
+    stack = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+    open = "({["
+    close = ")}]"
+    for c in s:
+        if c in open:
+            stack.append(c)
+        elif c in close:
+            if not stack or stack[-1] != pairs[c]:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
+
+if __name__ == "__main__":
+    print(is_valid("([)]"))
